bdc0cf63966582967244433f690c6ef5380c02bc
ajraj27 <rajanuj.iiita@gmail.com>
2018-10-20 14:54:46 +0530

    Its been a long day without you my friend

--- mp3.js
+++ mp3.js
@@ -5,24 +5,18 @@ var fs = require('fs');
 var io = require('socket.io')(server);
 var ss = require('socket.io-stream');
 
-app.use(express.static(`${__dirname}/html`));
+app.use(express.static(__dirname+'/html'));
 
 server.listen(8000);
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/html/indexmp3.html');
-});
-
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/html/index.html');
+// });
 
 io.on('connection', function (socket) {
-
-  socket.broadcast.emit('newMessage',"Hey!!!");
-  socket.emit('start', { hello: 'world' });
-  socket.on('stream', function (data) {
-    console.log(data);
     var stream = ss.createStream();
     var filename = __dirname + '/penningen.mp3' ;
-    ss(socket).emit('audio-stream', stream, { name: filename });
     fs.createReadStream(filename).pipe(stream);
-  });
+    ss(socket).emit('audio-stream', stream, { name: filename });
+
 });
